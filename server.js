23a11ecb43108086f40f1adbe58e3e60b88e22d5
nmt2501 <ngminhtuam@gmail.com>
2026-03-24 17:35:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
 
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/luck", async (req, res) => {
+    try {
+        const response = await axios.get("http://36.50.55.230:4784/luck8");
+
+        const data = response.data;
+
+        const result = {
+            phien: data.next_session,
+            du_doan: data.prediction,
+            do_tin_cay: data.reason
+        };
+
+        res.json(result);
+    } catch (err) {
+        res.status(500).json({
+            status: "error",
+            message: "Không lấy được API"
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
